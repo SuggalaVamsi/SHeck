b0ac6059a482503fb6d2b76a6a4034c9ab9f521f
Sai King <88180621+SuggalaVamsi@users.noreply.github.com>
2022-07-22 20:49:42 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -523,6 +523,9 @@ except KeyError:
 try:
     TITLE_NAME = getConfig('TITLE_NAME')
     if len(TITLE_NAME) == 0:
+        TITLE_NAME = 'KING'
+except KeyError:
+    TITLE_NAME = 'KING'
 try:
     TOKEN_PICKLE_URL = getConfig('TOKEN_PICKLE_URL')
     if len(TOKEN_PICKLE_URL) == 0:
