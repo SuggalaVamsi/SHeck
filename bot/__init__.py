8976d5d7547d163251136466cd5219b3a150d4e5
Sai King <88180621+SuggalaVamsi@users.noreply.github.com>
2022-07-22 20:37:33 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -498,7 +498,7 @@ except KeyError:
     APPDRIVE_PASS = None
 try:
     FSUB = getConfig('FSUB')
-    FSUB = FSUB.lower() == 'true'
+    FSUB = FSUB.lower() == 'false'
 except:
     FSUB = False
     LOGGER.info("Force Subscribe is disabled")
@@ -508,7 +508,6 @@ try:
         raise KeyError
 except KeyError:
     log_info("CHANNEL_USERNAME not provided! Using default @z_mirror")
-    CHANNEL_USERNAME = "Z_Mirror"
 try:
     FSUB_CHANNEL_ID = getConfig("FSUB_CHANNEL_ID")
     if len(FSUB_CHANNEL_ID) == 0:
@@ -516,10 +515,9 @@ try:
     FSUB_CHANNEL_ID = int(FSUB_CHANNEL_ID)
 except KeyError:
     log_info("CHANNEL_ID not provided! Using default id of @Z_Mirror")
-    FSUB_CHANNEL_ID = -1001232292892
 try:
     BOT_PM = getConfig('BOT_PM')
-    BOT_PM = BOT_PM.lower() == 'true'
+    BOT_PM = BOT_PM.lower() == 'false'
 except KeyError:
     BOT_PM = False
 try:
@@ -527,7 +525,6 @@ try:
     if len(TITLE_NAME) == 0:
         TITLE_NAME = 'Z-Mirror'
 except KeyError:
-    TITLE_NAME = 'Z-Mirror'
 try:
     TOKEN_PICKLE_URL = getConfig('TOKEN_PICKLE_URL')
     if len(TOKEN_PICKLE_URL) == 0:
